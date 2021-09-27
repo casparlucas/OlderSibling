9b80b84c3f9c43e2861fa1ba08e4ce8bc1d15cd6
Caspar <caspar.lucas@hotmail.com>
2021-09-27 14:55:31 +0100

    Refined code structure and comments added

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,20 +1,26 @@
-const insult = ['idiot','muppet','fool',]
+const insult = [
+    'idiot',
+    'muppet',
+    'fool'
+];
 const chore = [
     'wash the car',
     'mow the lawn',
     'tidy the living room',
     'clean the kitchen'
-]
+];
 const comment = [
     'and we can go to the cinema',
     'or we wont have time to game',
     'then we can relax'
-]
+];
+ 
+// insults, chores and comments may be added to above objects whilst within [] and seperated by ,
 
 function message() {
     var insultGiv = insult[Math.floor((Math.random())* insult.length)]
     var choreGiv = chore[Math.floor((Math.random())* chore.length)]
     var commentGiv = comment[Math.floor((Math.random())* comment.length)]
     return `Wake up ${insultGiv}! You have to ${choreGiv} today. Get it done ${commentGiv}.`
-}
+};
 console.log(message());
